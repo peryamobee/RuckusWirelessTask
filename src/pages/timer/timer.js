aeb2795f5c0a8eb9333032f4d963644e7b02e613
peryamobee <pery@amobee.com>
2016-01-31 04:25:07 +0200

    init commit round 5

---
+++ src/pages/timer/timer.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pery on 31/01/2016.
+ */
+module.exports = require("angular").module(__filename, [])
+
+function timerController(){
+
+}
+
+module.exports.stateConfig = {
+    name:"timer",
+    url:"/timer",
+    abstract:false,
+    template: require("./timer.html"),
+    controller: timerController,
+};
