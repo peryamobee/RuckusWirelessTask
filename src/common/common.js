f13a32d22575aac4518fd3babc80310a7a3385a5
peryamobee <pery@amobee.com>
2016-02-06 19:23:10 +0200

    timer basically working add some style

--- src/common/common.js
+++ src/common/common.js
@@ -1,8 +1,12 @@
 /**
  * Created by pery on 06/02/2016.
  */
+
+require('./styleFx/elegantShadow.scss')
+
 module.exports = angular.module(__filename, [
     require('./timer.drv/timer.drv.js').name
-]).run(function () {
+])
+    .run(function () {
     console.log('common run');
 });
--- src/common/timer.drv/timer.drv.js
+++ src/common/timer.drv/timer.drv.js
@@ -7,32 +7,98 @@ module.exports = angular.module(__filename,[])
     .run(function () {
         console.log('timer run');
     })
-    .directive('timer',[
-        function () {
-            return {
-                template:'10.10.10'
-                //,template:require('./timer.drv.html')
-                ,scope:{
-                    
-                }
-                ,controller: function ($scope) {
-                    function Timer(){
-                        ga('create', 'UA-XXXXX-Y', 'auto', 'myTracker');
+    .directive('timer',[function () {
+
+        var _duration = Symbol("duration");
+
+        return {
+            require:'timer'
+            ,template:require('./timer.drv.html')
+            ,controller: function ($scope) {
+                //ga('create', 'UA-XXXXX-Y', 'auto', 'myTracker');
+                var me = this;
+
+                reset();
+
+
+                /*API*/
+                return _.extend(me,{
+                    start: restart
+                    ,pause: pause
+                    ,reset: reset
+                    ,states:{
+                        paused:true
                     }
+                });
 
-                    Timer.prototype.start = function () {
-                        ga('create', 'UA-XXXXX-Y', 'auto', 'start timer');
-                    };
-
-                    Timer.prototype.stope = function () {
-                        ga('create', 'UA-XXXXX-Y', 'auto', 'stop timter');
-                        //_trackEvent('timer', 'start', time)
-                    };
+                function reset(){
+                    me[_duration] =  moment.duration($scope.duration);
                 }
-                ,link: function (scope, element, attr, ctrls ) {
 
+                function restart () {
+                    //ga('create', 'UA-XXXXX-Y', 'auto', 'start timer');
+                    reset();
+                    me.states.paused = false;
+                }
+
+                function pause () {
+                    //ga('create', 'UA-XXXXX-Y', 'auto', 'stop timter');
+                    //_trackEvent('timer', 'start', time)
+                    me.states.paused = true;
                 }
             }
+            ,controllerAs:'TimerCtrl'
+            ,scope:{
+                duration:'='
+                ,editor:'=?'
+
+            }
+            ,link: function (scope, element, attr, ctrl ) {
+                scope.editor = ctrl;
+
+
+                var $hours = element.find('.hours')
+                    ,$minutes = element.find('.minutes')
+                    ,$seconds = element.find('.seconds')
+                    ,miliSeconds = element.find('.mili-seconds')
+                ;
+                var previousCycle = Date.now()
+
+                ;
+                scope.$watch('duration',function(){
+                    ctrl.reset();
+                });
+
+
+                cycle();
+
+                function cycle(){
+                    var time = Date.now();
+                    if( !ctrl.states.paused ){
+                        updateTimer(time, previousCycle)
+                    }
+                    renderTimer(time, previousCycle);
+                    previousCycle = time;
+                    requestAnimationFrame(cycle);
+                }
+
+                function updateTimer(time,prevTime){
+                    var timePass = time - prevTime;
+                    var duration = ctrl[_duration];
+                    duration.subtract(timePass,'milliseconds');
+                }
+
+                function renderTimer(time,prevTime){
+                    var duration = ctrl[_duration];
+                    $hours.html( duration.hours() );
+                    $minutes.html( duration.minutes() );
+                    $seconds.html( duration.seconds() );
+                    miliSeconds.html( duration.milliseconds() );
+
+                }
+
+            }
+        }
             
     }]);
 
